1b21b9d18f66ad35b7331d500ce2223d9fc676b5
Joey <morri806@msu.edu>
2024-08-30 08:17:49 -0400

    Link

--- config/index.ts
+++ config/index.ts
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 
 export const siteConfig: Metadata = {
-  title: "Finance",
-  description: "Track your income and expenses with Finance.",
+  title: "Link",
+  description: "Track your income and expenses with Link.",
   keywords: [
     "reactjs",
     "nextjs",
@@ -45,11 +45,11 @@ export const siteConfig: Metadata = {
     "css",
   ] as Array<string>,
   authors: {
-    name: "Sanidhya Kumar Verma",
-    url: "https://github.com/sanidhyy",
+    name: "Joseph Morrison",
+    url: "https://github.com/jomo125",
   },
 } as const;
 
 export const links = {
-  sourceCode: "https://github.com/sanidhyy/finance-dashboard",
+  sourceCode: "https://github.com/jomo125/link",
 } as const;
