d224182effa46c9316c3576d2342d8c7dd259712
Joey <josephmorrison702@gmail.com>
2025-01-25 15:43:53 -0500

    j

--- app/(dashboard)/(account-filter)/transactions/r/[recurringId]/page.tsx
+++ app/(dashboard)/(account-filter)/transactions/r/[recurringId]/page.tsx
@@ -10,7 +10,6 @@ import { ArrowLeft } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { Textarea } from "@/components/ui/textarea";
 import {
   Select,
   SelectTrigger,
