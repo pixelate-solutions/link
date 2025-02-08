b8eae65825151d1b836ee913a5e52b250afea581
Joey <josephmorrison702@gmail.com>
2025-02-07 20:18:35 -0500

    j

--- app/(logic)/logic/page.tsx
+++ app/(logic)/logic/page.tsx
@@ -24,13 +24,13 @@ const LogicPage = () => {
           <TabsList className="sticky top-[220px] flex lg:w-full border border-gray-300 rounded-md mt-[130px] mb-[30px] lg:mt-[60px] lg:mb-[30px] z-50 mx-2 lg:mx-0">
             <TabsTrigger value="goals" className="w-1/3 text-center">Goals</TabsTrigger>
             <TabsTrigger value="main" className="w-1/3 text-center">Main</TabsTrigger>
-            <TabsTrigger value="chat" className="w-1/3 text-center">Chat</TabsTrigger>
+            <TabsTrigger value="chat" className="w-1/3 text-center">Forecast</TabsTrigger>
           </TabsList>
-            <div className="bg-gradient-to-br bg-white card lg:ml-[13%] lg:mr-[13%] h-[200px] rounded-b-2xl mx-2">
+            <div className="lg:ml-[13%] lg:mr-[13%] h-[200px] rounded-b-2xl mx-2">
 
                 <TabsContent value="goals" className="mt-4">Your goals content goes here.</TabsContent>
                 <TabsContent value="main" className="mt-4">Main content for Link Logic goes here.</TabsContent>
-                <TabsContent value="chat" className="mt-4">Chat content goes here.</TabsContent>
+                <TabsContent value="chat" className="mt-4">Forecast content goes here.</TabsContent>
                 
             </div>
           </Tabs>
