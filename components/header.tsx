6069b6397bf8f6da55d4af276b2eb5f8d9c8523d
Joey <josephmorrison702@gmail.com>
2025-02-17 10:31:38 -0500

    j

--- components/header.tsx
+++ components/header.tsx
@@ -35,7 +35,7 @@ export const Header = () => {
         {/* Shape 1 */}
         <div className="absolute -top-44 left-[40%] w-[200px] h-[400px] md:w-[400px] md:h-[700px] rotate-[40deg] bg-blue-400 opacity-30 rounded-full blur-3xl" />
         {/* Shape 2 */}
-        <div className="absolute top-[400px] right-0 w-[200px] h-[400px] md:w-[400px] md:h-[800px] rotate-[45deg] bg-blue-400 opacity-30 rounded-full blur-3xl overflow-hidden" />
+        <div className="absolute top-[400px] right-0 w-[200px] h-[400px] md:w-[400px] md:h-[800px] rotate-[45deg] bg-blue-400 opacity-30 rounded-full blur-3xl" />
         {/* Shape 3 */}
         <div className="absolute top-[10%] left-10 w-[200px] h-[400px] md:w-[400px] md:h-[400px] bg-purple-400 opacity-30 rounded-full blur-3xl" />
         {/* Shape 4 */}
