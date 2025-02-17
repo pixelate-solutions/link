8172e4770bcbbd6c8184780c8c9f1d0273bb4285
Joey <josephmorrison702@gmail.com>
2025-02-17 17:14:51 -0500

    j

--- components/header-landing.tsx
+++ components/header-landing.tsx
@@ -43,8 +43,8 @@ export const HeaderLanding = () => {
             <ClerkLoading>
               <ColorRing
                 visible={true}
-                height="80"
-                width="80"
+                height="40"
+                width="40"
                 ariaLabel="color-ring-loading"
                 wrapperStyle={{}}
                 wrapperClass="color-ring-wrapper"
